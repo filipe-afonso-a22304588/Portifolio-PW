3b61b66112299328159cc1116d52f320eb1a80a6
filipe-afonso-22304588 <161496584+filipe-afonso-a22304588@users.noreply.github.com>
2026-04-04 15:02:14 +0000

    criar entidade docente

--- project/portifolio/admin.py
+++ project/portifolio/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from .models import Docente
 
-# Register your models here.
+admin.site.register(Docente)
+
+class DocenteAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'link_clicavel')
+
+    def link_clicavel(self, obj):
+        return format_html('<a href="{}" target="_blank">Abrir</a>', obj.link)
--- project/portifolio/models.py
+++ project/portifolio/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Docente (models.Model):
+    nome = models.CharField(max_length=200, null=False, blank=False)
+    linkedin = models.URLField(blank=True)
+    
+    def __str__(self):
+        return self.nome
+
+
