95488ad1e792fe525e8e621a4de68988b2e29943
filipe-afonso-22304588 <161496584+filipe-afonso-a22304588@users.noreply.github.com>
2026-04-04 15:13:16 +0000

    Entidades Empresa e competencias

--- project/portifolio/admin.py
+++ project/portifolio/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.utils.html import format_html
-from .models import Docente
+from .models import Docente, Competencias, Empresa
 
 admin.site.register(Docente)
+admin.site.register(Competencias)
+admin.site.register(Empresa)
 
 class DocenteAdmin(admin.ModelAdmin):
     list_display = ('nome', 'link_clicavel')
--- project/portifolio/models.py
+++ project/portifolio/models.py
@@ -7,4 +7,26 @@ class Docente (models.Model):
     def __str__(self):
         return self.nome
 
+class Competencias (models.Model):
+    titulo = models.CharField(max_length=50,null=False,blank=False)
+    resumo_basico = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.titulo
+
+class Empresa (models.Model):
+    nome = models.CharField(max_length=100, null = False, blank=False)
+    data_comeco = models.DateField(null=False,blank=False)
+    data_saida = models.DateField()
+    competencias = models.ManyToManyField(Competencias)
+
+    def __str__(self):
+        return self.nome
+
+    
+
+
+
+
+
 
