72d5a5bbff9304692c6c347a23a03db47f7092bc
filipe-afonso-22304588 <161496584+filipe-afonso-a22304588@users.noreply.github.com>
2026-04-05 21:49:53 +0000

    correção de atributos não obrigatorios e exemplos

--- project/portifolio/models.py
+++ project/portifolio/models.py
@@ -9,7 +9,7 @@ class Docente (models.Model):
 
 class Competencia (models.Model):
     titulo = models.CharField(max_length=50,null=False,blank=False)
-    resumo_basico = models.CharField(max_length=200)
+    resumo_basico = models.CharField(max_length=200, blank=True)
 
     def __str__(self):
         return self.titulo
@@ -17,7 +17,7 @@ class Competencia (models.Model):
 class Empresa (models.Model):
     nome = models.CharField(max_length=100, null = False, blank=False)
     data_comeco = models.DateField(null=False,blank=False)
-    data_saida = models.DateField()
+    data_saida = models.DateField(null=True, blank=True)
     competencias = models.ManyToManyField(Competencia)
 
     def __str__(self):
@@ -26,10 +26,10 @@ class Empresa (models.Model):
 class Formacao (models.Model):
     titulo = models.CharField(max_length=100, null=False, blank=False)
     data = models.DateField(null=False, blank=False)
-    resumo = models.CharField(max_length=200)
+    resumo = models.CharField(max_length=200, blank=True)
 
     def __str__(self):
-        return self.nome
+        return self.titulo
     
 class TFC (models.Model):
     titulo = models.CharField(max_length=100, null=False, blank=False)
@@ -37,39 +37,53 @@ class TFC (models.Model):
     docente_responsavel = models.ManyToManyField(Docente, null=False, blank= False)
     tecnologias_usadas = models.CharField(max_length=100, null=False, blank=False)
     resumo = models.CharField(max_length=500, null=False, blank=False)
-    video_imagem = models.URLField()
+    video_imagem = models.URLField(blank=True)
     interesse = models.DecimalField(max_digits=2, decimal_places=1, null= False, blank=False)
 
+    def __str__(self):
+        return self.titulo
 
 class Tecnologia (models.Model):
     nome = models.CharField(max_length=100, null=False, blank=False)
     descricao =  models.CharField(max_length=200, null = True)
     exemplos_uso =  models.CharField(max_length=200, null=True)
     docente = models.ManyToManyField(Docente, null=False, blank=False)
-    logo = models.ImageField(upload_to='logos/', null=True)
+    logo = models.ImageField(upload_to='logos/', null=True,  blank=True)
+
+    def __str__(self):
+        return self.nome
 
 class Projeto (models.Model):
     titulo = models.CharField(max_length=100, null=False, blank=False)
     docentes = models.ManyToManyField(Docente, null=False, blank=False)
-    nota_final = models.DecimalField(max_digits=2, decimal_places=1, null= False, blank=False)
+    nota_final = models.DecimalField(max_digits=3, decimal_places=1, null= False, blank=False)
     tecnologias_usadas = models.ManyToManyField(Tecnologia, null=False,blank=False)
     descricao = models.CharField(max_length=200)
-    exemplo = models.ImageField(upload_to='projetos/', null=True)
-    link_deisi = models.URLField(null=True)
+    exemplo = models.ImageField(upload_to='projetos/', null=True, blank=True)
+    link_deisi = models.URLField(null=True,  blank=True)
+
+    def __str__(self):
+        return self.titulo
 
 class UC (models.Model):
     nome = models.CharField(max_length=100, null=False, blank=False)
     docentes = models.ManyToManyField(Docente, null=False,blank=False)
     tecnologias_aprendidas = models.ManyToManyField(Tecnologia, null=False,blank=False)
-    projeto_final = models.OneToOneField(Projeto, null=True, on_delete=models.SET_NULL)
+    projeto_final = models.OneToOneField(Projeto, null=True, on_delete=models.SET_NULL, blank=True)
     resumo = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome
 
 class Licenciatura (models.Model):
     nome = models.CharField(max_length=100, null=False, blank=False)
     ucs = models.ManyToManyField(UC, null=False,blank=False)
-    objetivos = models.CharField(max_length=500)
+    objetivos = models.CharField(max_length=500, blank=True)
     docentes = models.ManyToManyField(Docente, null=False, blank=False)
     tecnologias = models.ManyToManyField(Tecnologia, null=False, blank=False)
+
+    def __str__(self):
+        return self.nome
 
     
 
