9444ae5215fc953c5d746991aee5f21cc8b74367
filipe-afonso-22304588 <161496584+filipe-afonso-a22304588@users.noreply.github.com>
2026-04-22 14:57:13 +0000

    view professor

--- project/escola/urls.py
+++ project/escola/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
    path("cursos/", views.cursos_view, name='cursos'),
+   path("professores/", views.professores_view, name='professores'),
    path('', views.cursos_view),   # 
 ]
--- project/escola/views.py
+++ project/escola/views.py
@@ -1,10 +1,16 @@
 ##  ficheiro escola/views.py
 
 from django.shortcuts import render
-from .models import Curso
+from .models import Curso,Professor,Aluno
 
 def cursos_view(request):
 
     cursos = Curso.objects.select_related('professor').prefetch_related('alunos').all()
     
     return render(request, 'escola/curso.html', {'cursos': cursos})
+
+def professores_view(request):
+
+    professores = Professor.objects.all()
+    
+    return render(request, 'escola/professor.html', {'professores': professores})
