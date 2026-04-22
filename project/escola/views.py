dd3b660e3952950805e6485648ddfb373a7e1e8b
filipe-afonso-22304588 <161496584+filipe-afonso-a22304588@users.noreply.github.com>
2026-04-22 15:11:23 +0000

    viewalunos

--- project/escola/views.py
+++ project/escola/views.py
@@ -7,10 +7,20 @@ def cursos_view(request):
 
     cursos = Curso.objects.select_related('professor').prefetch_related('alunos').all()
     
-    return render(request, 'escola/curso.html', {'cursos': cursos})
+    return render(request, 'escola/cursos.html', {'cursos': cursos})
 
 def professores_view(request):
 
     professores = Professor.objects.all()
     
     return render(request, 'escola/professor.html', {'professores': professores})
+
+def alunos_view(request):
+
+    alunos = Aluno.objects.all()
+    
+    return render(request, 'escola/aluno.html', {'alunos': alunos})
+
+def curso_view(request, id):
+    curso=Curso.objects.get(id=id)       
+    return render(request, 'escola/curso.html', {'curso': curso})
